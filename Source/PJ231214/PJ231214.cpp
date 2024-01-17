09d0e87e6085ab3394a7860a7961971b1021854d
jinly73 <jinlee0703@gmail.com>
2024-01-17 16:29:33 +0900

    P38_Blueprint

---
+++ Source/PJ231214/PJ231214.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PJ231214.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PJ231214, "PJ231214" );
